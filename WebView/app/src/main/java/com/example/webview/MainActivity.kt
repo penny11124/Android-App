4e496ad337c74d302ec7da8d744414d3030285c9
penny <penny11124@gmail.com>
2023-07-12 02:49:33 +0800

    WebView

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var webView : WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.web)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.google.com")
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
