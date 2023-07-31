cfc4f88687a0838d196bc4305cd1d7f303157d06
penny <penny11124@gmail.com>
2023-07-31 16:56:19 +0800

    List View

---
+++ ListView/app/src/main/java/com/techmania/listview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.techmania.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String countries[];
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        countries = getResources().getStringArray(R.array.countries);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,countries);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String countryName = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"You selected "+countryName,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
