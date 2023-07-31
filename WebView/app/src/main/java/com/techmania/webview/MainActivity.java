deb09c8ba96a5f5ee3101d8d55b3be9283904ec0
penny <penny11124@gmail.com>
2023-07-31 16:57:37 +0800

    Web View

---
+++ WebView/app/src/main/java/com/techmania/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.techmania.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+    }
+}
